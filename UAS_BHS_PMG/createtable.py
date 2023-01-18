dd6683dc358f8bef1ac996e549ce2ec533454561
sulistiyawati27 <sulistyw29@student.esaunggul.ac.id>
2023-01-18 22:00:09 +0700

    pembuatan database

---
+++ UAS_BHS_PMG/createtable.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="172.18.102.103",
+  port=23306,
+  user="root",
+  password="p455w0rd",
+  database="basis_data"
+)
+
+db = mydb.cursor()
+
+db.execute("CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), nim VARCHAR(255))") 
