a370073288d3d8c5d18618fbbda564a8366e5fe4
sulistiyawati27 <sulistyw29@student.esaunggul.ac.id>
2023-01-18 21:37:26 +0700

    Contoh GUI dengan Tkinter

---
+++ UAS_BHS_PMG/GUI.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def on_button_click():
+    label.config(text="Hello, Tkinter!")
+
+root = tk.Tk()
+root.title("Contoh GUI dengan Tkinter")
+
+button = tk.Button(root, text="Klik Saya", command=on_button_click)
+button.pack()
+
+label = tk.Label(root)
+label.pack()
+
+root.mainloop()
